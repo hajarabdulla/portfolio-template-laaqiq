98ef5634026ad3b1da28df4c09d718c87c6471f6
hajarabdullayeva <hajar.pa@code.local>
2022-08-29 09:14:40 +0400

    bug fixed

--- src/app.js
+++ src/app.js
@@ -18,12 +18,8 @@ toggleBtn.addEventListener("click", (e) => {
   if (e.target.src !== "http://127.0.0.1:5500/src/images/up.svg") {
     description.classList.add("active");
     e.target.src = "../src/images/up.svg";
-    console.log(e.target);
   } else {
     description.classList.remove("active");
-    console.log(e.target);
     e.target.src = "../src/images/down.svg";
   }
-
-  console.log(e.target.src);
 });
